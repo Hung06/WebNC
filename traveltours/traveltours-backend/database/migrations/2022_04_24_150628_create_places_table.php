2618f4eeca47517c381acbc4994c8f897af4ddad
Hung06 <96820031+Hung06@users.noreply.github.com>
2024-06-13 16:05:19 +0700

    Hung data

---
+++ traveltours/traveltours-backend/database/migrations/2022_04_24_150628_create_places_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('province', 255);
+            $table->string('district', 255);
+            $table->string('address_detail', 500);
+            $table->string('name', 255);
+            $table->string('description');
+            $table->json('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
---
+++ traveltours/traveltours-backend/database/migrations/2022_04_24_151434_create_tours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('source');
+            $table->integer('dest');
+            $table->integer('range');
+            $table->timestamp('start_date');
+            $table->integer('slot');
+            $table->string('vehicle', 255);
+            $table->integer('hotel_star');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}
