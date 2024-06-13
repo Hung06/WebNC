c6165f1945c15e6a3cccae7e52ca441ca8396d7a
VuxQuang <vuthanhquang30032003@gmail.com>
2024-06-13 15:21:26 +0700

    frontend-folder

---
+++ traveltours/traveltours-frontend/src/common/config.js
@@ -0,0 +1,9 @@
+export const APP_API_URL =
+  process.env.VUE_APP_API_URL || 'http://localhost:8000/api/';
+export const APP_TIME_OUT = process.env.VUE_APP_TIME_OUT || 2000;
+
+export const APP_GLOBAL_TRANSITION = 'scroll-y-transition';
+
+export const APP_ITEMS_PER_PAGE = 15;
+
+export const APP_FILTER_MAX_PRICE = 1050;
