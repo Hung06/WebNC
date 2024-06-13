97a61b77050b95ad5b20b91079220cb73e29057d
Hung06 <96820031+Hung06@users.noreply.github.com>
2024-06-13 16:12:41 +0700

    Update User.php

--- traveltours/traveltours-backend/app/Models/User.php
+++ traveltours/traveltours-backend/app/Models/User.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    const ACTIVE = 'active';
+    const LOCKED = 'locked';
 
     /**
      * The attributes that are mass assignable.
@@ -20,7 +24,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+           'password',
+        'status',
+        'email',
+        'username',
+        'avatar'
     ];
 
     /**
@@ -41,4 +49,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
