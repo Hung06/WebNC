41a5bf800df7e3889338de5fd916c020fa905db3
VuxQuang <vuthanhquang30032003@gmail.com>
2024-06-15 16:39:27 +0700

    CardName

---
+++ traveltours/traveltours-frontend/src/components/Tours/Card/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <H3TBaseCard class="grid-card" style="max-width: 400px">
+    <div class="card-header">
+      <router-link :to="'/tour/' + tour.id">
+        <v-img
+          aspect-ratio="1.4"
+          class="white--text align-end rounded-tr-xl"
+          :src="tour.dest.images[0]"
+        >
+          <H3TSavingRibbon
+            :price="tour.price.adult"
+            :old-price="tour.price.adult + 10"
+        /></v-img>
+        <H3TCardName :name="tour.dest.name" />
+      </router-link>
+    </div>
+
+    <H3TQuickFacts :tour="tour" style="font-size: 14px" v-on="$listeners" />
+  </H3TBaseCard>
+</template>
+
+<script>
+  import H3TBaseCard from '@/components/base/Card';
+  import H3TCardName from './CardName.vue';
+  import H3TSavingRibbon from './SavingRibbon.vue';
+  import H3TQuickFacts from './quickFacts.vue';
+
+  export default {
+    components: {
+      H3TSavingRibbon,
+      H3TCardName,
+      H3TBaseCard,
+      H3TQuickFacts,
+    },
+
+    props: {
+      tour: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .card-header {
+    position: relative;
+    margin-bottom: 2em;
+  }
+</style>
