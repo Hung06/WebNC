30d33fe1480f8f3d246122c6f9247a3e7d5c1a55
Hung06 <96820031+Hung06@users.noreply.github.com>
2024-06-17 23:58:46 +0700

    Tours/Card updatee

--- traveltours/traveltours-frontend/src/components/Tours/Card/Price.vue
+++ traveltours/traveltours-frontend/src/components/Tours/Card/Price.vue
@@ -6,17 +6,12 @@
 
     <v-col cols="10">
       <TrnSkewText class="mt-n3 pr-2">
-        <div class="text-h6 primary--text text--lighten-1 px-0">$</div>
-        <div class="text-h4 primary--text text--lighten-1 pr-3">
+        
+        <div class="text-h6 primary--text text--lighten-1 pr-3">
           {{ priceFormatted(price) }}
         </div>
+        <div class="text-h6 primary--text text--lighten-1 px-0">Đồng</div>
 
-        <span
-          v-if="oldPrice"
-          class="strikeout text-h6 secondary--text text--darken-1 px-0"
-        >
-          ${{ priceFormatted(oldPrice) }}
-        </span>
       </TrnSkewText>
     </v-col>
   </v-row>
