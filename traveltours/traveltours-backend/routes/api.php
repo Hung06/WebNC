99d7c01c31f3ff9632ed5b1cf0ddf7713150f593
Hung06 <96820031+Hung06@users.noreply.github.com>
2024-06-17 23:50:14 +0700

    Update api.php

--- traveltours/traveltours-backend/routes/api.php
+++ traveltours/traveltours-backend/routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\EmailController; // Add this line
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController; // Add this line
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/send-email', [EmailController::class, 'sendEmail']);
+
+Route::post('send-email', [EmailController::class, 'sendEmail']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -36,6 +38,8 @@ Route::group([
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/google', [GoogleController::class, 'redirectToGoogle']);
+    Route::get('/google/callback', [GoogleController::class, 'handleGoogleCallback']);      
 });
 
 Route::group([
